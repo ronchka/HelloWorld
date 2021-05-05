a8a3d14bd92fd8c5dc4514f3b8db8358d5195b36
Natalia <ronchka2016@gmail.com>
2021-05-05 20:15:33 +0300

    updated Array.java

--- Array.java
+++ Array.java
@@ -4,12 +4,11 @@ public class Array {
     {
         int[] nums = {1,2,3,4,5};
         int last = nums[nums.length - 1];
-        int first = nums[nums.length - 5];
+        int first = nums[0];
         nums[0] = last;
         nums[nums.length-1] = first;
-        for (int i = 0; i <5; i++) {
-            System.out.print(nums[i]);
-        }
+        int sum = nums[0] + nums[nums.length/2];
+        System.out.println(sum);
     }
 
 }
